17f86d437f6b4eeee138fccf7d8e43ba75262abf
Shivanshu Prakash <shivanshuprakas02@gmail.com>
2026-01-09 23:44:29 +0530

    Types of data types

---
+++ 03/script.js
@@ -0,0 +1,18 @@
+// Types of data types 
+
+// String: Represents text data.
+// Number: Represents both integer and floating-point numbers using a 64-bit format. This includes special values like Infinity and NaN.
+// Boolean: Represents true or false for logical operations.
+// Undefined: Indicates a variable that has been declared but not assigned a value.
+// Null: Represents the intentional absence of an object value, assigned explicitly. typeof null is "object", which is a historical issue.
+// Symbol: A unique and immutable value used as object property identifiers.
+// BigInt: Represents integers of arbitrary size, larger than the standard Number type. 
+
+name = "Shivanshu "
+console.log(typeof name)
+
+let role 
+console.log(typeof role)
+// wierd but true 
+console.log(typeof null)
+
