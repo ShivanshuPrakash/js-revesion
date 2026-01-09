e60a722f4e5fce39762f2ef7274b256a8bb3ff72
Shivanshu Prakash <shivanshuprakas02@gmail.com>
2026-01-09 23:33:50 +0530

    variable refresher

---
+++ 02/script.js
@@ -0,0 +1,13 @@
+let name = "Shivanshu Prakash"
+var roll = 2520803123;
+let branch = "computer science"
+engineer = true 
+
+console.log(name);
+console.log(engineer)
+
+// instead of printing all again and again i can use table 
+console.table([name , roll , branch ,engineer])
+
+// var is not generally used to initialize the variable due to its scope is global 
+// let has local scope 
