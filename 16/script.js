807ad8588159ed13e5e98262a6dbcb502e46ac7e
Shivanshu Prakash <shivanshuprakas02@gmail.com>
2026-02-01 09:45:00 +0530

    working of Js

---
+++ 16/script.js
@@ -0,0 +1,25 @@
+//  JS execution context and call stack 
+
+// js is single threaded 
+
+
+//  JS execution context
+// gloabal EC
+// function  EC
+ // Eval EC
+
+// STEP 1 : Globas execution ---> allocation to this 
+// STEp 2 : Memory phase ---> keeping variables with undefined nature / function with there defination
+// (step 1 and step 2 are called phase 1)
+// STEP 3 : Execution Phase ---> for variables values releated to them are executed / for functions
+//  function will execute but they will make there new execution context means its own memory and execution phase 
+// the return call in function are done on global executional context 
+
+ // java script code run in 2 phase 
+//  Memory creation phase  --> only variables/functions without values are stored  
+// execution phase  --> during time of execution phase the values are assigned to varibalse that are stored in meomry execution phase 
+
+// Queestions
+// 1.  What is new variable envoriment (timespan - 17:39)
+// 2. What do u mean by threads and wht do you understand by singlethreaded 
+// 3. what is callstack 
